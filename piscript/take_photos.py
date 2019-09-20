76dd1338d49ab83b3945e0f4432b77d2f7db7bbe
Shane Folden <footballshane@gmail.com>
2019-09-20 08:22:20 -0700

    Update take_photos.py

--- piscript/take_photos.py
+++ piscript/take_photos.py
@@ -3,6 +3,8 @@ import time
 import picamera
 import subprocess
 
+# runs linux command as subprocess to delete all files in minitruckee/images older than 2 hours
+# prvents storage issues
 def clean_file():
     print("cleaning")
     delete_files = subprocess.check_output(["find", "/home/pi/minitruckee/images", "-type", "f",  "-mmin", "+120", "-delete"])
@@ -19,8 +21,10 @@ with picamera.PiCamera() as camera:
     print("go")
     clean_file()
     time.sleep(.1)
+    
     while True:
         timestr = time.strftime("%m-%d--%H:%M:%S")                                                                                                                                                            
         file_str = "/home/pi/minitruckee/images/"+timestr+"{}.jpg".format(i)
+        #Set file name to current date + i that way pictures in the same second dont get overwritten
         camera.capture(file_str)
         i = i + 1
